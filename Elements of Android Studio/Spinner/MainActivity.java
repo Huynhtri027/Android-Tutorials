9c2d08fec01dc3dfd802a2e45ab7e6b9c7fb82f6
Kerem Oflu <keremoflu92@gmail.com>
2017-12-06 01:35:24 +0300

    Add files via upload

---
+++ Elements of Android Studio/Spinner/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.kerem.spinnerdeneme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Spinner spinner;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spinner = (Spinner)findViewById(R.id.spinner);
+        textView = (TextView)findViewById(R.id.textView_chosenValue);
+
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this,R.array.days_item, android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinner.setAdapter(adapter);
+
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String[] daysList = getResources().getStringArray(R.array.days_item);
+                textView.setText(daysList[i]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+}
