32404aee4b0cf3bb2c69388e6ce23cdb044eb6ca
Kerem Oflu <keremoflu92@gmail.com>
2018-05-25 23:11:18 +0300

    Add files via upload

---
+++ Animations/Shared.java
@@ -0,0 +1,44 @@
+package com.example.keremoflu.lottie;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class Shared extends AppCompatActivity {
+
+    private ImageView image1;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shared);
+
+        //
+        image1 = findViewById(R.id.image1);
+        textView = findViewById(R.id.text1);
+        //
+
+        image1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Shared.this, Shared2.class);
+
+                Pair[] pairs = new Pair[2];
+                pairs[0] = new Pair<View, String>(image1, "transition_image");
+                pairs[1] = new Pair<View, String>(textView, "transition_text");
+
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(Shared.this, pairs);
+
+                startActivity(intent, options.toBundle());
+            }
+        });
+    }
+}
